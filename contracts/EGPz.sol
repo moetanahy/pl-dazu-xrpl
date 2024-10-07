3b096e43cbcdaee346cd2a8ba7c28fe8e35c359b
Mahamad El Tanahy <moe@brightcreations.com>
2024-10-07 15:34:26 -0600

    adjusting this

--- contracts/EGPz.sol
+++ contracts/EGPz.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract EGPz is ERC20 {
     constructor() ERC20("EGPz", "EGPz") {
-        // Mint 1,000,000 USDz tokens to the contract deployer
+        // Mint 1,000,000 EGPz tokens to the contract deployer
         _mint(msg.sender, 1_000_000 * 10 ** decimals());
     }
 }
