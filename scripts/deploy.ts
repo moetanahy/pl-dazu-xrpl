e8781a11d3775f9b4347244bdd6c678fb17c8402
Mahamad El Tanahy <moe@brightcreations.com>
2024-10-07 16:25:46 -0600

    cleaning up deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,17 +11,6 @@ async function main() {
   console.log(`Account balance: ` + balance);
   // Rest of your deployment code...
 
-  // // Prepare deployments args to be passed to the constructor
-  // const unlockTime = Math.floor(Date.now() / 1000) + 60*5; // 5 minutes in the future
-  // const value_im_locking = ethers.parseEther("10"); // Parsing ETH/XRP value into WEI (1ETH = 10^18wei)
-  // console.log("Locking 10 XRP");
-  // console.log("Unlocking time: "+unlockTime);
-
-  // const Lock = await ethers.getContractFactory("Lock");
-  // const lock = await Lock.deploy(unlockTime, { value: value_im_locking });
-
-  // console.log("Lock contract deployed to:", lock.target);
-
   const USDz = await ethers.getContractFactory("USDz");
   const USD = await USDz.deploy();
 
